80c71c764a2dce1501c06d3a272fbb040ea32f1d
Volodyadr1923-new <132037514+Volodyadr1923-new@users.noreply.github.com>
2023-05-01 14:29:09 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ def test_button_test_for_different_site_languages(browser):
     try:
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         browser.get(link)
-        time.sleep(15)
+        time.sleep(30)
         browser.implicitly_wait(1)
         enter = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
         assert len(enter) != 0, "The button is missing from the page!"
