f58d42d64a67c74952ebe3911e1926e911efdc7a
Volodyadr1923-new <vkadimirmakshin@gmail.com>
2023-05-01 14:06:12 +0300

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,18 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+def pytest_addoption(parser):
+    parser.addoption('--language')
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    # для запроса значения параметра вызовем команду:
+    language_name = request.config.getoption("--language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages': language_name})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
